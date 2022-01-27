c47bde86ae19cc7f5981d5580cf80e81e50cb986
Somak Goswami <92353322+toastedjames@users.noreply.github.com>
2022-01-27 23:39:22 +0530

    Add files via upload

---
+++ Diary/exit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Exit Function
+int exit1(){
+    system("cls");
+    fflush(stdin);
+    printf("Exit Function Is Activated\n\n\n");
+    printf("              ________\n");
+    printf("             / ______ \\\n");
+    printf("             || _  _ ||\n");
+    printf("             ||| || |||\n");
+    printf("             |||_||_|||\n");
+    printf("             || _  _o||\n");
+    printf("             ||| || |||\n");
+    printf("             |||_||_|||      ^~^  ,\n");
+    printf("             ||______||     \('Y') )\n");
+    printf("            /__________\\    /   \\/\n");
+    printf("    ________|__________|__ \(\\|||/) _________\n");
+    printf("           /____________\\\n");
+    printf("           |____________|\n\n");
+    printf("The System Will Be Closing Now\n");
+    system("pause");
+}
